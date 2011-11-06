c72fee3b3aaba78f2f60f4dec4900feda3a15501
Siraaj Khandkar <siraaj@khandkar.net>
2011-11-05 23:37:32 -0400

    Abstracted random price and symbol into functions.

--- ticker.erl
+++ ticker.erl
@@ -3,16 +3,40 @@
 
 
 start() ->
-    CharPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
-    DigPool = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
-    LenPool = [1, 2, 3],
-
-    Symbol = [choice(CharPool) || S <- lists:seq(1, 3)],
-    Price = [choice(DigPool) || S <- lists:seq(1, choice(LenPool))],
+    Price = random_price(),
+    Symbol = random_symbol(),
 
     Output = [[{symbol, Symbol}, {price, Price}]],
 
     io:format("~p~n", Output).
+
+
+random_symbol() ->
+    CharPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
+    Symbol = [choice(CharPool) || S <- lists:seq(1, 3)],
+    Symbol.
+
+
+random_price() ->
+    DigPool = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+    LenPool = [1, 2, 3],
+
+    Dollars = [
+        integer_to_list(choice(DigPool)) ||
+        S <- lists:seq(1, choice(LenPool))
+    ],
+
+    Cents = [
+        integer_to_list(choice(DigPool)) ||
+        S <- lists:seq(1, 2)
+    ],
+
+    Price = string:join(
+        [string:join(Dollars, ""), string:join(Cents, "")],
+        "."
+    ),
+
+    list_to_float(Price).
 
 
 %%-----------------------------------------------------------------------------
