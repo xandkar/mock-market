7fd774da5a8ed9e9d49d08f9eef163d5df6cfeac
Siraaj Khandkar <siraaj@khandkar.net>
2011-11-06 19:29:52 -0500

    for() function replacing unidiomatic list comprehensions.

--- market.erl
+++ market.erl
@@ -10,7 +10,7 @@ start() ->
     MaxListings = 5,
     MaxBrokers = 3,
     Listings = sets:to_list(sets:from_list(
-        [random_symbol() || Stock <- lists:seq(1, MaxListings)]
+        for(1, MaxListings, fun() -> random_symbol() end)
     )),
 
     register(ticker, spawn(market, ticker, [])),
@@ -72,26 +72,26 @@ broker(Listings, Ticker, Interval) ->
 %%% Helper functions
 %%%----------------------------------------------------------------------------
 
+%% Generates a random stock symbol
 random_symbol() ->
     CharPool = "ABCDEFGHIJKLMNOPQRSTUVWXYZ",
     LenPool = [1, 2, 3, 4],
-    Symbol = [choice(CharPool) || S <- lists:seq(1, choice(LenPool))],
+    Symbol = for(1, choice(LenPool), fun() -> choice(CharPool) end),
     Symbol.
 
 
+%% Generates a random price
 random_price() ->
     DigPool = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
     LenPool = [1, 2, 3],
 
-    Dollars = [
-        integer_to_list(choice(DigPool)) ||
-        S <- lists:seq(1, choice(LenPool))
-    ],
+    Dollars = for(1, choice(LenPool),
+        fun() -> integer_to_list(choice(DigPool)) end
+    ),
 
-    Cents = [
-        integer_to_list(choice(DigPool)) ||
-        S <- lists:seq(1, 2)
-    ],
+    Cents = for(1, 2,
+        fun() -> integer_to_list(choice(DigPool)) end
+    ),
 
     Price = string:join(
         [string:join(Dollars, ""), string:join(Cents, "")],
@@ -106,3 +106,11 @@ choice(List) ->
     Maximum = length(List),
     Element = random:uniform(Maximum),
     lists:nth(Element, List).
+
+
+%% Repeat Function(), Max times, accumulating a list of its return values
+for(Max, Max, Function) ->
+    [Function()];
+
+for(Init, Max, Function) ->
+    [Function() | for(Init + 1, Max, Function)].
