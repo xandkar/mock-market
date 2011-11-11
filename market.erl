7ce33cf2b0b3227701a43f0fc4fd192e5e5b21a6
Siraaj Khandkar <siraaj@khandkar.net>
2011-11-11 17:53:00 -0500

    Aligns nicer this way :)

--- market.erl
+++ market.erl
@@ -1,9 +1,9 @@
 -module(market).
 -compile(export_all).
 
+-define(TICKER_INTERVAL, 1000).
 -define(NUM_LISTINGS, 5).
 -define(NUM_BROKERS, 3).
--define(TICKER_INTERVAL, 1000).
 
 
 %%%----------------------------------------------------------------------------
