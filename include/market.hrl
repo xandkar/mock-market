3b4eef8068c84da645fe4dba4788b8054d1d3f22
Siraaj Khandkar <siraaj@khandkar.net>
2011-11-12 17:07:18 -0500

    Making sure data dir is created at project root.

--- include/market.hrl
+++ include/market.hrl
@@ -3,7 +3,7 @@
 -define(NUM_BROKERS, 3).
 -define(MAX_SHARES_PER_TRANSACTION, 10).
 -define(LOG_FIELD_DELIMITER, "\t").
--define(PATH_DIR__DATA, "data").
+-define(PATH_DIR__DATA, "../data").
 -define(PATH_FILE__LOG,
     string:join([?PATH_DIR__DATA, "transactions.log"], "/")
 ).
